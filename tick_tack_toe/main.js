3980fb01d46defe3aebe31d5badb91977e384a50
Jocelyn Wensloff <jewensloff@gmail.com>
2023-06-06 18:27:03 -0600

    feat: Add a function that creates the player

---
+++ tick_tack_toe/main.js
@@ -0,0 +1,17 @@
+// query selectors 
+
+// event listeners
+
+// global variables 
+var players = []
+
+// functions 
+
+function createPlayerObject(player, numberOfWins){
+  return{
+    player: player,
+    wins: numberOfWins,
+    turn: false,
+  }
+}
+
