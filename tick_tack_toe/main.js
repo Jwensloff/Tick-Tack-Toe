6372aa105e1efc6a1595e94cbffeecfa6529079e
Jocelyn Wensloff <jewensloff@gmail.com>
2023-06-08 13:36:30 -0600

    feat: Add an event listener to game board that also toggles the player

--- tick_tack_toe/main.js
+++ tick_tack_toe/main.js
@@ -1,13 +1,35 @@
 // query selectors 
-// var gameBoardGrid = document.querySelector
-// event listeners
+var gameBoard = document.querySelector('.wrapper');
+
+var activeTurnDisplay = document.querySelector('h3');
+
+var pirateImage = document.createElement('img');
+var ninjaImage = document.createElement('img');
+// Set the source (src) attribute to your PNG image file
+pirateImage.src = 'assets/pirate.png';
+ninjaImage.src = 'assets/ninja.png';
+
+// Append the <img> element as a child of the <h3> element
+// heading.appendChild(image);
+
+
+gameBoard.addEventListener('click', function(event) {
+  if (currentPlayer === 'pirate'){
+      pirate.moves.push(event.target.id)
+      currentPlayer = 'ninja'
+  } else {
+    ninja.moves.push(event.target.id)
+    currentPlayer = 'pirate'
+  }
+  console.log(currentPlayer) 
+});
 
 // global variables 
 var allMovesPlayed = []
 
-var pirate = createPlayerObject('pirate', 'X');
+var pirate = createPlayerObject('pirate', '');
 var ninja = createPlayerObject('ninja', 'O');
-var players = {pirate, ninja}
+var currentPlayer = 'pirate'
 
 // functions 
 
@@ -32,13 +54,16 @@ function updateGameBoard(cellNumber){
 }
 
 function addPlayerMoves(player, cellNumber){
+  isTurn(player)
   if(allMovesPlayed.includes(cellNumber)) {
     return
   }
-  if(players[player].id == player){
-    players[player].moves.push(cellNumber)
+  if(currentPlayer.id === player){
+    currentPlayer.moves.push(cellNumber)
   }
   joinAllMovesPlayed();
+  togglePlayerTurn(player)
+  togglePlayerTurnDisplay(player)
   return players 
 }
 
@@ -47,8 +72,48 @@ function joinAllMovesPlayed(){
   return allMovesPlayed
 }
 
+function isTurn(player){
+  currentPlayer.turn = true
+  return players
+}
 
-// create a function that keeps track of game board 
-  // iterate through game board? 
-  // should game booard be the array that holds objects?
+function togglePlayerTurn(player){
+  if(currentPlayer === 'ninja'){
+    players.ninja.turn = false
+    players.pirate.turn = true 
+  } else {
+    players.pirate.turn = false
+    players.ninja.turn = true
+  }
+  return players
+}
 
+// if ninja.turn = true 
+// click event -->
+  // that means the ninja just played a move 
+      // ninja.turn = false
+      // pirate.turn = true 
+      
+// 'event'.
+
+
+//  if one player.turn = true, other is false 
+// if currentPlayer === ninja
+      // currentPlayer.turn = true 
+      // currentPlayer.turn = false 
+// if currentPlayer === pirate 
+      // currentPlayer.turn = true 
+
+  
+  // ninja.turn === true
+    // pirate.turn = false
+  // if pirate.turn === true
+    // ninja.turn = false
+
+
+    function togglePlayerTurnDisplay(player){
+      activeTurnDisplay.innerText=''
+      if(currentPlayer.turn === true){
+        activeTurnDisplay.innerText= `It's ${player.token} turn`
+      }
+    }
