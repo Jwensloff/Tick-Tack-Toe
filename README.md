d357de806ba70433239ff5871008cfad02c32f91
Jocelyn Wensloff <jewensloff@gmail.com>
2023-06-14 11:03:48 -0600

    Transfer entire project into the proper repository

---
+++ README.md
@@ -0,0 +1,54 @@
+# Tick Tack Toe: 
+## Pirates vs. Ninjas edition 
+
+### Abstract
+
+This project allows users to enjoy a classic game of tick tack toe with a themed twist. A heading above the board will toggle to indicate which players turn it is. If a player wins a round, the banner above the board will indicate which player won, and the number of wins will be tracked and displayed below the player icon on either side of the game's board. Lastly, if there is a draw, neither player's score will increase, and the header above the board will indicate that the outcome was a draw.   
+
+### Installation instructions
+
+To clone down this repository to your local machine:
+* Fork the Tick-Tack-Toe repository
+* Clone down the repository to your local machine
+* `cd` into the repository on your local machine
+* Open the repository in your text editor
+* From there you should be able to open the Javascript, CSS, and HTML files, as well as the functioning appplication through a web browser using the command `open index.html`. 
+
+### Preview of App:
+
+<img width="1433" alt="Screenshot 2023-06-12 at 10 52 33 AM" src="https://github.com/reneepinna/ideaBox/assets/61986277/bb6242d7-28d7-4734-bb70-bb9c92d36321">
+
+
+### Context:
+
+Tick tack toe is a classic game, but this application was a solo coding challenge for students who are in the 5th week of Mod 1 at the Turing School of Software and Design. Students were alloted 7 days to complete this project. 
+
+### Contributors:
+- Jocelyn Wensloff - https://github.com/Jwensloff
+
+
+### Learning Goals:
+
+- Solidify and demonstrate your understanding of:
+  - DRY JavaScript
+  - event delegation to handle similar event listeners
+
+- Understand the difference between the data model and how the data is displayed on the DOM
+
+- Iterate through/filter DOM elements using for loops
+
+- Use your problem solving process to break down large problems, solve things step by step, and trust yourself to not rely on an outside “answer” to a logical challenge
+
+
+### Wins 
+
+- Completing the project on time with a day to spare
+
+- Writting the code for functionality that resets the game board without creating any bugs. 
+
+
+### Challenges:
+
+- The biggest challenge was starting the project. This was the first project that we didn't have a set of instructions to follow, so decifing on a data model was very challenging. 
+
+- The function I struggled the most with was the checkForDraw function, but after I talked through it with a mentor I'm very happy with the function I ended up with. 
---
+++ main.js
@@ -0,0 +1,149 @@
+// query selectors 
+var gameBoardGrid = document.querySelector('.wrapper');
+var displayPlayerTurn = document.querySelector('h2');
+var pirateWins = document.querySelector('.pirate-wins');
+var ninjaWins = document.querySelector('.ninja-wins');
+var gameBoardCells = document.querySelectorAll('.cell');
+
+// event listeners
+gameBoardGrid.addEventListener('click', function(event) {
+  if (gameBoard[parseInt(event.target.id)] === parseInt(event.target.id)) {
+    if (allowClick === false){
+      return;
+    }
+    updateGameBoard(event);
+    checkForWinCondition();
+  }
+});
+
+// global variables 
+var pirateImg = '<img class="current-player-image" src="assets/pirate.png" alt="Skull and swords" />';
+var ninjaImg = '<img class="current-player-image" src="assets/ninja.png" alt="Ninja silhouette" />';
+
+var pirate = createPlayerObject('pirate', pirateImg, pirateWins);
+var ninja = createPlayerObject('ninja', ninjaImg, ninjaWins);
+
+var gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+var currentPlayer = pirate;
+var startingPlayer = pirate;
+var allowClick = true;
+
+// functions 
+
+function createPlayerObject(player, token, htmlElement){
+  return {
+    id: player,
+    token: token,
+    numWinsDisplay: htmlElement,
+    wins: 0,
+  }
+}
+
+function increaseWins(currentPlayer){
+  currentPlayer.wins +=1;
+
+  currentPlayer.numWinsDisplay.innerText = '';
+  currentPlayer.numWinsDisplay.innerText = `${currentPlayer.wins} wins`;
+
+  return currentPlayer;
+}
+
+function togglePlayerTurn(){
+  currentPlayer = (currentPlayer === pirate) ? ninja : pirate; 
+  togglePlayerTurnDisplay();
+}
+
+function togglePlayerTurnDisplay(){
+  displayPlayerTurn.HTML='';
+  if (currentPlayer === pirate){
+    return displayPlayerTurn.innerHTML = `It's ${pirateImg} turn`;
+  } 
+    return displayPlayerTurn.innerHTML = `It's ${ninjaImg} turn`;
+}
+
+function updateGameBoard(event){
+  for (var i = 0; i < gameBoard.length; i++){
+    if (parseInt(event.target.id) === gameBoard[i]){
+      gameBoard.splice(i, 1, currentPlayer.id);
+    }
+  }
+  addPlayerTokenToGameBoard(event);
+  return gameBoard;
+}
+
+function addPlayerTokenToGameBoard(event){
+  event.target.innerHTML = currentPlayer.token;
+}
+
+function announceWinnerAndEndGame(){
+  allowClick = false;
+  displayPlayerTurn.innerHTML = '';
+  displayPlayerTurn.innerHTML = `The ${currentPlayer.id}'s have won this battle.`;
+  increaseWins(currentPlayer);
+  setTimeout(resetBoard, 4000);
+}
+
+// 
+function checkForWinCondition(){   
+  if (gameBoard[0] === gameBoard[1] && gameBoard[1] === gameBoard[2]) {
+    announceWinnerAndEndGame();
+    return;
+  } else if (gameBoard[3] === gameBoard[4] && gameBoard[4] === gameBoard[5]) {
+    announceWinnerAndEndGame();
+    return;
+  } else if (gameBoard[6] === gameBoard[7] && gameBoard[7] === gameBoard[8]) {
+    announceWinnerAndEndGame();
+    return;
+  } else if (gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6]) {
+    announceWinnerAndEndGame();
+    return; 
+  } else if (gameBoard[1] === gameBoard[4] && gameBoard[4] === gameBoard[7]) {
+    announceWinnerAndEndGame();
+    return; 
+  } else if (gameBoard[2] === gameBoard[5] && gameBoard[5] === gameBoard[8]) {
+    announceWinnerAndEndGame();
+    return; 
+  } else if (gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8]) {
+    announceWinnerAndEndGame();
+    return; 
+  } else if (gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6]) {
+    announceWinnerAndEndGame();
+    return; 
+  } else {
+    if (checkForDraw() === false){
+    togglePlayerTurn(currentPlayer);
+    }
+  }
+}
+
+function checkForDraw(){
+  var count = 0; 
+
+  for (var i = 0; i < gameBoard.length; i++){
+    if (gameBoard.includes(i)) {
+      count += 1;
+    } 
+  } 
+  
+  if (count === 0){
+    displayPlayerTurn.innerHTML = `It's a draw.`;
+    setTimeout(resetBoard, 4000);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function resetBoard(){
+  gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+  for (var i = 0; i < gameBoardCells.length; i++){
+    gameBoardCells[i].innerHTML='';
+  }
+
+  startingPlayer = (startingPlayer === pirate) ? ninja : pirate; 
+  currentPlayer = startingPlayer;
+  allowClick = true;
+  togglePlayerTurnDisplay();
+}
